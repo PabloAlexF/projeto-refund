d82f2e8b6f7323cad1112b8851e7dde0a29c7970
Pablo <Pablolexandrade@gmail.com>
2025-09-25 19:59:15 -0300

    Manipulando o dom adicionando o nome, categoria e imagem correspondente ao selecionar.

---
+++ script.js
@@ -0,0 +1,76 @@
+const form = document.querySelector("form");
+const amount = document.getElementById("amount");
+const expense = document.getElementById("expense")
+const category = document.getElementById("category")
+
+// Selecionando os elementos da lista:
+const ExpenseList = document.querySelector("ul");
+
+
+amount.oninput = () => {
+    let value = amount.value.replace(/\D/g, "")
+
+    value = Number(value) / 100;
+
+    amount.value = formatCurrencyBRL(value);
+}
+
+function formatCurrencyBRL (value) {
+
+    return value.toLocaleString("pt-BR", {
+        style: "currency",
+        currency: "BRL",
+    })
+
+}
+
+form.onsubmit = (event) => {
+    event.preventDefault()
+
+    const newExpense = {
+        id: new Date().getTime(),
+        expense: expense.value,
+        category_id: category.value,
+        category_name: category.options[category.selectedIndex].text,
+        amount: amount.value,
+        created_at: new Date(),
+    }
+    expenseAdd(newExpense)
+}
+
+function expenseAdd(newExpense) {
+    try {
+        
+        const expenseItem = document.createElement("li");
+        expenseItem.classList.add("expense");
+
+        const expenseIcon = document.createElement("img");
+        expenseIcon.setAttribute("src", `img/${newExpense.category_id}.svg`)
+        expenseIcon.setAttribute("alt", newExpense.category_name)
+        expenseIcon.classList.add("iconslist");
+
+        // Adicionando a informação da despesa:
+
+        const expenseInfo = document.createElement("div");
+        expenseInfo.classList.add("expense-info");
+
+        const expenseName = document.createElement("strong");
+        expenseName.textContent = newExpense.expense;
+
+        const expenseCategory = document.createElement("span");
+        expenseCategory.textContent = newExpense.category_name;
+
+
+        // adicionando os items na lista:
+        ExpenseList.append(expenseItem)
+        expenseItem.append(expenseIcon)
+        expenseItem.append(expenseInfo);
+        expenseInfo.append(expenseName);
+        expenseInfo.append(expenseCategory);
+
+    } catch (error) {
+        alert("Não foi possível atualizar a lista de despesas.")
+        console.log(error)
+    }
+}
+
